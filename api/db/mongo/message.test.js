6e5a66e066f7a51a343796e244bdfacb30112f08
ramyelkest <4thkest@gmail.com>
2016-11-01 00:37:59 +0000

    missed the tests.. DoH!

---
+++ api/db/mongo/message.test.js
@@ -0,0 +1,116 @@
+/* eslint func-names:0 no-underscore-dangle: 0 */
+
+import { assert } from 'chai';
+import { Connection, Message } from '..';
+
+
+describe('Message database model', () => {
+  before(function () {
+    this.timeout(10000); // might have to wait for the db to start
+    return Connection;
+  });
+
+  beforeEach(() => {
+    return Message.remove({});
+  });
+
+  it('should contain no messages', () => {
+    return Message.find().exec()
+    .catch(() => {
+      assert.ok(false);
+    })
+    .then((doc) => {
+      assert.equal(doc.length, 0);
+    });
+  });
+
+  it('should error when name is not defined', () => {
+    return new Message().save()
+    .then((doc) => {
+      assert.notOk(doc);
+    })
+    .catch((error) => {
+      assert.equal(error.errors.content.kind, 'required');
+      assert.equal(error.errors.postedBy.kind, 'required');
+    });
+  });
+
+  it('should insert message into the database', () => {
+    return new Message({
+      threadId: 'testThreadId',
+      content: 'testContent',
+      postedBy: 'testUser',
+    })
+    .save()
+    .then((doc) => {
+      assert.isObject(doc._id);
+      assert.isNumber(doc.createdAt.getTime());
+      assert.isNumber(doc.updatedAt.getTime());
+      assert.equal(doc.threadId, 'testThreadId');
+      assert.equal(doc.content, 'testContent');
+      assert.equal(doc.postedBy, 'testUser');
+    });
+  });
+
+  it('should retrieve all messages by thread id', () => {
+    return Promise.all([
+      new Message({
+        threadId: 't_1',
+        content: 'content1',
+        postedBy: 'user1',
+      }).save(),
+      new Message({
+        threadId: 't_1',
+        content: 'content2',
+        postedBy: 'user2',
+      }).save(),
+      new Message({
+        threadId: 't_2',
+        content: 'content3',
+        postedBy: 'user2',
+      }).save(),
+    ])
+    .then(() => {
+      return Message.find().exec().then((doc) => {
+        assert.equal(doc.length, 3);
+      });
+    })
+    .then(() => {
+      Message.getByThreadId('t_1').then((doc) => {
+        assert.equal(doc.length, 2);
+        assert.equal(doc[0].threadId, 't_1');
+        assert.equal(doc[1].threadId, 't_1');
+      });
+    })
+    .then(() => {
+      Message.getByThreadId('t_2').then((doc) => {
+        assert.equal(doc.length, 1);
+        assert.equal(doc[0].threadId, 't_2');
+      });
+    });
+  });
+
+  it('should remove message from the database', () => {
+    return new Message({
+      threadId: 'testThreadId',
+      content: 'testContent',
+      postedBy: 'testUser',
+    })
+    .save()
+    .then(() => {
+      return Message.remove({ threadId: 'testThreadId' })
+      .then((doc) => {
+        assert.equal(doc.result.n, 1);
+      });
+    })
+    .then(() => {
+      return Message.find().exec()
+      .catch(() => {
+        assert.ok(false);
+      })
+      .then((doc) => {
+        assert.equal(doc.length, 0);
+      });
+    });
+  });
+});
---
+++ api/db/mongo/thread.test.js
@@ -0,0 +1,131 @@
+/* eslint func-names:0 no-underscore-dangle:0, arrow-parens:0 */
+
+import { assert } from 'chai';
+import { Connection, Thread } from '..';
+
+
+describe('Thread database model', () => {
+  before(function () {
+    this.timeout(10000); // might have to wait for the db to start
+    return Connection;
+  });
+
+  beforeEach(() => {
+    return Thread.remove({});
+  });
+
+  it('should contain no threads', () => {
+    return Thread.find().exec()
+    .catch(() => {
+      assert.ok(false);
+    })
+    .then((doc) => {
+      assert.equal(doc.length, 0);
+    });
+  });
+
+  it('should error when name is not defined', () => {
+    return new Thread().save()
+    .then((doc) => {
+      assert.notOk(doc);
+    })
+    .catch((error) => {
+      assert.equal(error.errors.name.kind, 'required');
+    });
+  });
+
+  it('should retrieve a thread by id', () => {
+    return new Thread({
+      name: 'test',
+      readBy: ['testUser'],
+    })
+    .save()
+    .then((doc) => {
+      return doc.id;
+    })
+    .then(() => {
+      Thread.getById('N/A').then((doc) => {
+        assert.notOk(doc);
+      });
+    })
+    .then((id) => {
+      Thread.getById(id).then((doc) => {
+        assert.equal(doc.length, 1);
+        assert.equal(doc[0].name, 'test');
+      });
+    });
+  });
+
+  it('should retrieve a threads by ids', () => {
+    const ids = [];
+    return Promise.all([
+      new Thread({
+        name: 'thread 1',
+        readBy: ['user1'],
+      }).save().then((doc) => ids.push(doc.id)),
+      new Thread({
+        name: 'thread 2',
+        readBy: ['user2'],
+      }).save().then((doc) => ids.push(doc.id)),
+      new Thread({
+        name: 'thread 3',
+        readBy: ['user1'],
+      }).save().then((doc) => ids.push(doc.id)),
+    ])
+    .then(() => {
+      assert(ids.length, 3);
+    })
+    .then(() => {
+      Thread.getByIds([]).then((doc) => {
+        assert.equal(doc.length, 0);
+      });
+    })
+    .then(() => {
+      Thread.getByIds([ids[0]]).then((doc) => {
+        assert.equal(doc.length, 1);
+      });
+    })
+    .then(() => {
+      Thread.getByIds(ids).then((doc) => {
+        assert.equal(doc.length, 3);
+      });
+    });
+  });
+
+  it('should insert thread into the database', () => {
+    return new Thread({
+      name: 'test',
+      readBy: ['testUser'],
+    }).save().then((doc) => {
+      assert.isObject(doc._id);
+      assert.isNumber(doc.createdAt.getTime());
+      assert.isNumber(doc.updatedAt.getTime());
+      assert.equal(doc.name, 'test');
+      assert.equal(doc.readBy.length, 1);
+      assert.equal(doc.readBy[0], 'testUser');
+    });
+  });
+
+  it('should remove thread from the database', () => {
+    return new Thread({
+      name: 'test',
+      readBy: ['testUser'],
+    })
+    .save()
+    .then(() => {
+      return Thread.remove({ name: 'test' })
+      .then((doc) => {
+        assert.equal(doc.result.n, 1);
+      });
+    })
+    .then(() => {
+      return Thread.find().exec()
+      .catch(() => {
+        assert.ok(false);
+      })
+      .then((doc) => {
+        assert.equal(doc.length, 0);
+      });
+    });
+  });
+});
